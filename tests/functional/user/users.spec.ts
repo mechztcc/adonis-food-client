39bdf84d36fe5fda5246cd1b0df8b135d25d968d
mechztcc <dojathan@gmail.com>
2022-08-02 10:45:00 -0400

    feat: defined user model / migration

--- tests/functional/user/users.spec.ts
+++ tests/functional/user/users.spec.ts
@@ -1,5 +1,11 @@
 import { test } from '@japa/runner'
 
 test.group('Users users', () => {
-  // Write your test here
+  test('It should be create a user', async ({ client }) => {
+    const response = await client
+      .post('/users')
+      .json({ name: 'Alberto Paiva', phone: '55 9 9999-9999', password: '123456' })
+
+    response.assertStatus(201)
+  })
 })
