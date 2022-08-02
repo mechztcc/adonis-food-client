0d54de052c00b4af6c67167097ac8d41f1123b71
mechztcc <dojathan@gmail.com>
2022-08-02 10:59:18 -0400

    feat: add test to validade when try to create a user with no required fields

--- tests/functional/user/users.spec.ts
+++ tests/functional/user/users.spec.ts
@@ -13,4 +13,13 @@ test.group('Users users', () => {
       password: '123456',
     })
   })
+
+  test('It should be return 422 when do not provide required fields', async ({ client }) => {
+    const response = await client
+      .post('/users')
+      .json({ name: 'Alberto Paiva', phone: '55 9 9999-9999' })
+
+    response.assertBodyContains({ errors: [] })
+    response.assertStatus(422)
+  })
 })
